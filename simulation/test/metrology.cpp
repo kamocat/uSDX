a0b4f1d530c8cc04b7d603c45af405f70b634b9b
Marshal Horn <kamocat@gmail.com>
2020-10-24 15:13:19 -0700

    Update metrology.cpp

--- simulation/test/metrology.cpp
+++ simulation/test/metrology.cpp
@@ -3,15 +3,18 @@
 #include <vector>
 
 static enum state{
-  pos_inc = 0,
-  pos_dec,
-  neg_inc,
-  neg_dec
+  pos_inc = 0b00,
+  pos_dec = 0b01,
+  neg_inc = 0b10,
+  neg_dec = 0b11
 };
 
 /* Finds peaks and zero-crossings.
    Returns the number of nodes found.
  */
 int find_nodes( vector<float> nodes, vector<float> y){
+  short state = pos_inc;
   for(auto i = y.begin(); i != y.end()-1; ++i ){
-    
+    state <<= 2;
+    state |= y[i]<0.0 ? neg_inc : pos_inc;
+    state |=
