cd9aafb3cbd956a89370a031b7fb15ebcbe42054
Marshal Horn <kamocat@gmail.com>
2020-10-24 16:14:35 -0700

    Update metrology.cpp

--- simulation/test/metrology.cpp
+++ simulation/test/metrology.cpp
@@ -9,6 +9,19 @@ static enum state{
   pos = 0b01
 };
 
+/* Estimates the peak between equally spaced points y1, y2, and y3.
+ * Returns a value between 0 and 2.
+ */
+float estimate_peak(float y1, float y2, float y3){
+  float x;
+  if(y1>y3){
+    x = 1 + 0.5*(y3-y1)/(y2-y1);
+  } else {
+    x = 1 - 0.5*(y3-y1)/(y2-y3);
+  }
+  return x;
+}
+
 /* Finds peaks and zero-crossings.
    Returns the number of nodes found.
  */
@@ -16,7 +29,8 @@ int find_nodes( vector<float> nodes, vector<float> y){
   short state = 0;
   state |= y[0]<0.0 ? neg : pos;
   state |= y[1]>y ? inc : dec;
-  for(auto i = y.begin()+1; i != y.end()-2; ++i ){
+  //FIXME: y.begin() is an iterator, not an index
+  for(auto i = y.begin()+1; i != y.end()-1; ++i ){
     state |= y[i]<0.0 ? neg : pos;
     state |= y[i+1]>y ? inc : dec;
     if( (state>>2 ^ state) & dec){ // sign changed
@@ -26,6 +40,8 @@ int find_nodes( vector<float> nodes, vector<float> y){
     }
     if( (state>>2 ^ state) & neg){ // Inflection point
       // Estimate peak. We know it's beteen i+1 and i-1
+      float x = estimate_peak(y[i-1], y[i], y[i+1]);
+      nodes.push_back(x+i);
     }
   }
   return nodes.length();
